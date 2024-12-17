16aeb09274c17f7bdbbf40fd90850d41434e51c8
Jeff Sell <jtsell@gmail.com>
2024-12-17 18:43:03 -0500

    Implemented solution for part b of three

--- src/three.clj
+++ src/three.clj
@@ -1,12 +1,27 @@
 (ns three
- (:require
-  [util.input :refer [get-input!]]))
+  (:require
+   [clojure.string :as str]
+   [util.input :refer [get-input!]]))
 
-(defn- format-input [input]
+(defn- parse-muls [input]
   (->> input
        (re-seq #"mul\((\d+),(\d+)\)")
        (map rest)
        (map (fn [[a b]] [(Integer/parseInt a) (Integer/parseInt b)]))))
 
-(defn solve []
-  (reduce (fn [acc [a b]] (+ acc (* a b))) 0 (format-input (get-input! 3))))
+(defn solve-a []
+  (reduce (fn [acc [a b]] (+ acc (* a b))) 0 (parse-muls (get-input! 3))))
+
+(defn- keep-enabled [input]
+  (let [split (str/split input #"don't\(\)")]
+    (println (first split))
+    (->> split
+         (map (fn [s] (str/split s #"do\(\)")))
+         (map rest)
+         (remove empty?)
+         flatten
+         (concat (first split))
+         (apply str))))
+
+(defn solve-b []
+  (reduce (fn [acc [a b]] (+ acc (* a b))) 0 (parse-muls (keep-enabled (get-input! 3)))))

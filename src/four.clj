065faf687850f512e492d6f09fcf9eca5ba43382
Jeff Sell <jtsell@gmail.com>
2024-12-17 19:13:57 -0500

    Reorganized day four

--- src/four.clj
+++ src/four.clj
@@ -34,3 +34,21 @@
     (reduce (fn [acc coordinates] (+ acc (check-initial matrix coordinates "X")))
             0
             (flatten-coordinates matrix))))
+
+(defn- diagonal-edge-set [matrix coordinates [d1 d2]]
+  (set [(get-in-matrix matrix (get-in-direction matrix d1 coordinates))
+        (get-in-matrix matrix (get-in-direction matrix d2 coordinates))]))
+
+(defn- check-cross [matrix coordinates]
+  (if (and (= "A" (get-in-matrix matrix coordinates))
+           (= #{"M" "S"}
+              (diagonal-edge-set matrix coordinates [:sw :ne]) 
+              (diagonal-edge-set matrix coordinates [:nw :se])))
+    1
+    0))
+
+(defn solve-b []
+  (let [matrix (string->matrix (get-input! 4))]
+    (reduce (fn [acc coordinates] (+ acc (check-cross matrix coordinates)))
+            0
+            (flatten-coordinates matrix))))
